077cb60b95cc422804eca70878b54c88e1ef51cc
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2022-12-15 06:49:14 +1100

    remove surplus default buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,26 +36,6 @@ export default function Home() {
             <h2>Public Decks &rarr;</h2>
             <p>Browse decks that others have shared</p>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
