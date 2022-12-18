e30362f932e675ee630cfe23bedcbda12aa66daa
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2022-12-18 15:04:44 +1100

    update login component

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,43 +1,81 @@
 import {
   FormControl,
   FormLabel,
-  FormHelperText,
   Input,
   Button,
   VStack,
+  HStack,
   Flex,
-  Box,
   useToast,
   Heading,
-  Link,
+  SimpleGrid,
+  GridItem,
+  Checkbox,
 } from "@chakra-ui/react";
 
 function LoginForm() {
   const toast = useToast();
 
   return (
-    <VStack as="form" justifyContent="center" bg="gray.200">
-      <FormControl>
-        <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input id="email" type="email" placeholder="Enter your email address" />
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel htmlFor="password">Password</FormLabel>
-        <Input
-          id="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-      </FormControl>
-      <Flex mt={4} justifyContent="center" alignItems="center">
-        <Button type="submit">Log in</Button>
-      </Flex>
-      <FormControl>
-        <Link>Forgot password</Link>
-        <Button variant="outline" onClick={console.log}>
-          Sign up
-        </Button>
-      </FormControl>
+    <VStack
+      w="full"
+      h="full"
+      p={10}
+      spacing={10}
+      alignItems="flex-start"
+      as="form"
+      justifyContent="center"
+      bg="gray.200"
+    >
+      <VStack spacing="{3}" alignItems="flex-start">
+        <Heading size="lg">Sign in</Heading>
+      </VStack>
+      <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              id="email"
+              type="email"
+              placeholder="Enter your email address"
+            />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl mt={4}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Checkbox>Remember me</Checkbox>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Button size="lg" w="full" type="submit">
+            Sign in
+          </Button>
+        </GridItem>
+        <HStack justifyContent="space-between" w="full">
+          <GridItem colSpan={1}>
+            <FormControl>
+              <Button variant="link">Forgot password</Button>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <FormControl>
+              <Button variant="outline" onClick={console.log}>
+                Sign up
+              </Button>
+            </FormControl>
+          </GridItem>
+        </HStack>
+      </SimpleGrid>
+
+      <Flex mt={4} justifyContent="center" alignItems="center"></Flex>
     </VStack>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,48 +2,56 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import LoginForm from "../components/loginForm";
+import { Container, Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Ballpark</title>
-        <meta name="description" content="ballpark app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Ballpark !</h1>
+    <Container maxW="container.x1" p={0}>
+      <Flex h="100vh" py={20}>
         <div>
-          <Image
-            src="/baseballonCard.png"
-            alt="baseball"
-            width={200}
-            height={200}
-          />
+          <Head>
+            <title>Ballpark</title>
+            <meta name="description" content="ballpark app" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+
+          <main>
+            <h1 className={styles.title}>Ballpark</h1>
+            <div>
+              <Image
+                src="/baseballonCard.png"
+                alt="baseball"
+                width={200}
+                height={200}
+              />
+            </div>
+
+            <p>A simple app to track your baseball card collection</p>
+
+            <div>
+              <LoginForm></LoginForm>
+            </div>
+          </main>
+
+          <footer className={styles.footer}>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{" "}
+              <span className={styles.logo}>
+                <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  width={72}
+                  height={16}
+                />
+              </span>
+            </a>
+          </footer>
         </div>
-
-        <p className={styles.description}>
-          A simple app to track your baseball card collection
-        </p>
-
-        <div className={styles.grid}>
-          <LoginForm></LoginForm>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+      </Flex>
+    </Container>
   );
 }
